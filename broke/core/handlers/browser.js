21595322d65070a2bd22765334cf3b61ae1989b3
Davide Callegari <davide@brokenseal.it>
2010-04-20 08:09:03 -0700

    First step to create the browser handler

--- broke/core/handlers/browser.js
+++ broke/core/handlers/browser.js
@@ -1,7 +1,15 @@
 (function(__global__){
-	var __module__ = broke.core.handlers.browser = {};
+	var __module__ = broke.core.handlers.browser = {},
+		BaseHandler= broke.core.handlers.base.BaseHandler,
+		HttpRequest= broke.http.HttpRequest;
 	
+	BrowserRequest= HttpRequest.extend("broke.core.handlers.browser.BrowserRequest", {
+		
+	});
 	
+	BaseHandler.extend("broke.core.handlers.browser.BrowserHandler", {
+		requestClass: BrowserRequest
+	});
 	
 	return __module__;
 })(this);
