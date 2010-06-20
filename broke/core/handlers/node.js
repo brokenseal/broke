476c94c4ed34927ba9456b34650416ad67e2da34
Davide Callegari <davide@brokenseal.it>
2010-06-20 17:00:50 +0200

    Node request and handler

--- broke/core/handlers/node.js
+++ broke/core/handlers/node.js
@@ -1,3 +1,23 @@
 (function(_){
+	var 
+		BaseHandler= require('broke/core/handlers/base').BaseHandler
+		,HttpRequest= require('broke/http/http').HttpRequest
+	;
 	
+	HttpRequest.extend({
+		meta: {
+			className: 'NodeRequest',
+			parent: _
+		}
+	});
+	
+	BaseHandler.extend({
+		meta: {
+			className: 'NodeHandler',
+			parent: _
+		},
+		prototype: {
+			requestClass: _.NodeRequest
+		}
+	});
 })(exports);
