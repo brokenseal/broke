0548e4c2d3558d61dd8fa2749946479757d88074
Davide Callegari <davide@brokenseal.it>
2010-06-20 17:00:15 +0200

    Right port of the Django modules

---
+++ broke/conf/global_settings.js
@@ -0,0 +1,139 @@
+(function(_){
+	var
+		__module__ = {
+			AJAX: {							// ajax settings
+				dataType: 'json',			// ajax data type which returns from the server
+				async: false
+			},
+			BASE_URL: '/media/broke',
+			
+			// cache system
+			// broke implements only a local storage cache backend, for now
+			CACHE_BACKEND: 'local_storage://',
+			CACHE_MIDDLEWARE_SECONDS: 600,
+			CACHE_MIDDLEWARE_KEY_PREFIX: '',
+			
+			DEBUG_PROPAGATE_EXCEPTIONS: false,
+			
+			DISALLOWED_USER_AGENTS: [],
+			
+			DEBUG: false,
+			CONTEXT_PROCESSORS: [
+				'broke.contextProcessors.debug'
+			],
+			
+			DATE_FORMAT: '%Y/%m/%d',
+			
+			FORCE_SCRIPT_NAME: null,
+			
+			GET_LATEST_BY: 'id',			// defines what field models' manager method
+											// getLatest will look at
+			
+			HANDLER_404: 'broke.views.defaults.pageNotFound',
+			
+			INSTALLED_APPS: [],
+			
+			JSON_URLS: {
+				'getData': '/%(appLabel)s/%(model)s/json/get_data/',
+				'save': '/%(appLabel)s/%(model)s/json/save/',
+				'delete': '/%(appLabel)s/%(model)s/json/delete/'
+			},
+			
+			// locale specific settings
+			LOCALE_PATHS: [],
+			LANGUAGE_CODE: 'en',
+			LANGUAGES: {
+				'en': 'English',
+				'it': 'Italian'
+			},
+			LANGUAGE_COOKIE_NAME: 'broke_language',
+			
+			MIDDLEWARE_CLASSES: [
+				'broke.middleware.common.CommonMiddleware'
+			],
+			
+			//preventDefault: false,		// whether you want the broke.response to
+											// prevent default action from the browser
+											// careful: hideHash and preventDefault are equivalent!!
+											// just use preventDefault for now
+			
+			PRELOAD_REMOTE_TEMPLATES: false,// wether you want Broke to preload your templates
+											// remotely
+											// if set to true, Broke will attempt to download
+											// your templates from a remote location
+											// based on the basePath of your app
+											// e.g.: if an app's baseDir is '/myProject/apps/blog'
+											// and your app has a 'templates' object looks like
+											// { 'entry-view.html': '' }, then Broke will try to
+											// download '/myProject/apps/blog/templates/entry-view.html'
+			
+			QUERY_TYPE: 'local',			// local|remote
+											// wether you want your query to hit a local database,
+											// populated with previously fetched data,
+											// or query the remote database
+			
+			REQUIRE: {
+				DYNAMIC: true,
+				TIMEOUT: 3600,
+				PATHS: []
+			},
+			
+			ROOT_URLCONF: null,				// a string representing the object's configuration
+			
+			STOP_PROPAGATION: false,		// whether you want the broke.response to
+											// prevent event propagation
+			
+			SAVE: {							// save settings
+				commit: true				// if you want to prevent the object to
+											// be saved right away, you should set this
+											// to false and then explicitly call the
+											// save method on the object with commit= true
+											// e.g.: entry.save({commit: true})
+			},
+			TEMPLATE_STRING_IF_INVALID: '',
+			TEMPLATE_PATHS: [],
+			TEMPLATE_LOADERS: [
+				'broke.template.loaders.apps',
+			],
+			
+			// list of elements that trigger the
+			// broke.request event, setting the url
+			// WARNING: will only work if 'eventTriggeringMethod' is set to 'elements'
+			URL_CHANGING_ELEMENTS: {
+				'a': {
+					events: ['click'],		// events
+					urlAttribute: 'href'	// attribute holding the url
+				},
+				'form': {
+					events: ['submit'],
+					urlAttribute: 'action',
+					preventDefault: true
+				}
+				// you may attach the jQ request event to different elements
+				// for instance you could do that:
+				//'div[data-href]': {
+				//	events: ['click'],
+				//	urlAttribute: 'data-href'	// html5 "data-" attribute
+				//},
+				// or even
+				//'*[data-href]': {
+				//	events: ['click'],
+				//	urlAttribute: 'data-href'
+				//}
+			},
+			USE_AJAX: true,					// would you like to use ajax?
+											// if false, it will create a form and
+											// send your date through that form
+											// WARNING: not ready yet!
+			
+			USE_I18N: false
+		}
+		,name
+	;
+	
+	for(name in __module__) {
+		if(__module__.hasOwnProperty(name)) {
+			_[name]= __module__[name];
+		}
+	}
+})(exports);
--- broke/conf/settings.js
+++ broke/conf/settings.js
@@ -1,138 +1,138 @@
 (function(_){
 	var
-		__module__ = {
-			AJAX: {							// ajax settings
-				dataType: 'json',			// ajax data type which returns from the server
-				async: false
-			},
-			BASE_URL: '/media/broke',
-			
-			// cache system
-			// broke implements only a local storage cache backend, for now
-			CACHE_BACKEND: 'local_storage://',
-			CACHE_MIDDLEWARE_SECONDS: 600,
-			CACHE_MIDDLEWARE_KEY_PREFIX: '',
-			
-			DEBUG_PROPAGATE_EXCEPTIONS: false,
-			
-			DISALLOWED_USER_AGENTS: [],
-			
-			DEBUG: false,
-			CONTEXT_PROCESSORS: [
-				'broke.contextProcessors.debug'
-			],
-			
-			DATE_FORMAT: '%Y/%m/%d',
-			
-			FORCE_SCRIPT_NAME: null,
-			
-			GET_LATEST_BY: 'id',			// defines what field models' manager method
-											// getLatest will look at
-			
-			HANDLER_404: 'broke.views.defaults.pageNotFound',
-			
-			INSTALLED_APPS: [],
-			
-			JSON_URLS: {
-				'getData': '/%(appLabel)s/%(model)s/json/get_data/',
-				'save': '/%(appLabel)s/%(model)s/json/save/',
-				'delete': '/%(appLabel)s/%(model)s/json/delete/'
-			},
-			
-			// locale specific settings
-			LOCALE_PATHS: [],
-			LANGUAGE_CODE: 'en',
-			LANGUAGES: {
-				'en': 'English',
-				'it': 'Italian'
-			},
-			LANGUAGE_COOKIE_NAME: 'broke_language',
-			
-			MIDDLEWARE_CLASSES: [
-				'broke.middleware.common.CommonMiddleware'
-			],
-			
-			//preventDefault: false,		// whether you want the broke.response to
-											// prevent default action from the browser
-											// careful: hideHash and preventDefault are equivalent!!
-											// just use preventDefault for now
-			
-			PRELOAD_REMOTE_TEMPLATES: false,// wether you want Broke to preload your templates
-											// remotely
-											// if set to true, Broke will attempt to download
-											// your templates from a remote location
-											// based on the basePath of your app
-											// e.g.: if an app's baseDir is '/myProject/apps/blog'
-											// and your app has a 'templates' object looks like
-											// { 'entry-view.html': '' }, then Broke will try to
-											// download '/myProject/apps/blog/templates/entry-view.html'
-			
-			QUERY_TYPE: 'local',			// local|remote
-											// wether you want your query to hit a local database,
-											// populated with previously fetched data,
-											// or query the remote database
-			
-			REQUIRE: {
-				DYNAMIC: true,
-				TIMEOUT: 3600,
-				PATHS: []
-			},
-			
-			ROOT_URLCONF: null,				// a string representing the object's configuration
-			
-			STOP_PROPAGATION: false,		// whether you want the broke.response to
-											// prevent event propagation
-			
-			SAVE: {							// save settings
-				commit: true				// if you want to prevent the object to
-											// be saved right away, you should set this
-											// to false and then explicitly call the
-											// save method on the object with commit= true
-											// e.g.: entry.save({commit: true})
-			},
-			TEMPLATE_STRING_IF_INVALID: '',
-			TEMPLATE_PATHS: [],
-			TEMPLATE_LOADERS: [
-				'broke.template.loaders.apps',
-			],
-			
-			// list of elements that trigger the
-			// broke.request event, setting the url
-			// WARNING: will only work if 'eventTriggeringMethod' is set to 'elements'
-			URL_CHANGING_ELEMENTS: {
-				'a': {
-					events: ['click'],		// events
-					urlAttribute: 'href'	// attribute holding the url
-				},
-				'form': {
-					events: ['submit'],
-					urlAttribute: 'action',
-					preventDefault: true
-				}
-				// you may attach the jQ request event to different elements
-				// for instance you could do that:
-				//'div[data-href]': {
-				//	events: ['click'],
-				//	urlAttribute: 'data-href'	// html5 "data-" attribute
-				//},
-				// or even
-				//'*[data-href]': {
-				//	events: ['click'],
-				//	urlAttribute: 'data-href'
-				//}
-			},
-			USE_AJAX: true,					// would you like to use ajax?
-											// if false, it will create a form and
-											// send your date through that form
-											// WARNING: not ready yet!
-			
-			USE_I18N: false
-		}
+		Class= require('dependencies/class').Class
+		,globalSettings= require('broke/conf/global_settings')
+		//,LazyObject= require('broke/utils/functional').LazyObject
+		,utils= require('broke/core/utils')
+		,gettext= require('dependencies/gettext').gettext.gettext
+		,exceptions= require('broke/core/exceptions')
+		,ImportError= exceptions.ImportError
+		,KeyError= exceptions.KeyError
+		,RunTimeError= exceptions.RunTimeError
+		,ENVIRONMENT_VARIABLE= 'BROKE_SETTINGS_MODULE'
+		,settings
 	;
 	
-	for(var name in __module__) {
-		if(__module__.hasOwnProperty(name)) {
-			_[name]= __module__[name];
+	/*
+	A lazy proxy for either global Django settings or a custom settings object.
+	The user can manually configure settings prior to using them. Otherwise,
+	Django uses the settings module pointed to by DJANGO_SETTINGS_MODULE.
+	*/
+	/*LazyObject.extend({
+		meta: {
+			className: 'LazySettings'
+			,parent: _
 		}
-	}
+		,prototype: {
+			_setup: function(){
+				// Load the settings module pointed to by the environment variable. This
+				// is used the first time we need any settings at all, if the user has not
+				// previously configured the settings manually.
+				var
+					settingsModule= global[ENVIRONMENT_VARIABLE]
+				;
+				
+				if(ENVIRONMENT_VARIABLE in global && settingsModule === undefined) {
+					throw new KeyError();
+				} else {
+					throw new ImportError(gettext('Settings cannot be imported, because global variable %s is undefined." % ENVIRONMENT_VARIABLE').echo(ENVIRONMENT_VARIABLE));
+				}
+				
+				this._wrapped= _.Settings(settingsModule);
+			}
+			,configure: function(defaultSettings, options){
+				// Called to manually configure the settings. The 'default_settings'
+				// parameter sets where to retrieve any unspecified values from.
+				var
+					holder
+				;
+				
+				if(this._wrapped) {
+					throw new RunTimeError(gettext('Settings already configured.'));
+				}
+				
+				holder= UserSettingsHolder(defaultSettings);
+				
+				utils.extend(holder, options);
+				
+				this._wrapped= holder;
+			}
+			,configured: function(){
+				// Returns True if the settings have already been configured.
+				
+				return utils.bool(this._wrapped);
+			}
+		}
+	});
+	*/
+	
+	Class.extend({
+		meta: {
+			className: 'Settings'
+			,parent: _
+		}
+		,prototype: {
+			init: function(settingsModule){
+				// update this object from global settings (but only for ALL_CAPS settings)
+				var
+					_this= this
+					,mod
+				;
+				
+				utils.forEach(globalSettings, function(name){
+					if(name == name.toUpperCase()) {
+						_this[name]= this;
+					}
+				});
+				
+				// store the settings module in case someone later cares
+				this.SETTINGS_MODULE= settingsModule;
+				
+				mod= require(settingsModule);
+				
+				if(!mod) {
+					throw new ImportError(gettext("Could not import settings '%s' (Is it on sys.path? Does it have syntax errors?)".echo(this.SETTINGS_MODULE)));
+				}
+				
+				// I'm not going to translate the tuple_settings because I strongly believe it's a mistake
+				
+				utils.forEach(mod, function(name){
+					if(name == name.toUpperCase()) {
+						_this[name]= this;
+					}
+				});
+				
+				// Expand entries in INSTALLED_APPS like "django.contrib.*" to a list
+				// of all those apps.
+				// TODO
+				
+				// TIME_ZONE stuff
+				// TODO
+			}
+		}
+	});
+	
+	// Holder for user configured settings.
+	Class.extend({
+		meta: {
+			className: 'UserSettingsHolder'
+			,parent: _
+		}
+		,prototype: {
+			init: function(defaultSettings){
+				// Requests for configuration variables not in this class are satisfied
+				// from the module specified in default_settings (if possible).
+				
+				this.defaultSettings= defaultSettings;
+			}
+		}
+	});
+	
+	// settings= new _.LazySettings(); -> can't to that in Javascript (?)
+	settings= new _.Settings(global[ENVIRONMENT_VARIABLE]);
+	
+	utils.extend(_, {
+		ENVIRONMENT_VARIABLE: ENVIRONMENT_VARIABLE
+		,settings: settings
+	});
 })(exports);
